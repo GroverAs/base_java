d593e527dd42837a1aedc08519fce2dd04521b53
Alexander Savelev <alex.savelev21@yandex.ru>
2023-05-31 23:37:07 +0500

    hw3

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -15,12 +15,20 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void fillDeletedResume(int index) {
-
+        int deletedInd = size-index-1;
+        if (deletedInd < 0){
+            System.arraycopy(storage,????????);    //застрял здесь
+        }
 
     }
 
     @Override
     protected void addResume(Resume resume, int index) {
-
+        int addedInd = -index - 1;
+        System.arraycopy(storage, addedInd, storage, index + 1, size - addedInd);
+        storage[addedInd] = resume;
+        size++;
     }
+
 }
+
