0bd45014e4fbcf41bae2fe29f2c8c33bee04ba79
Alexander Savelev <alex.savelev21@yandex.ru>
2023-06-13 12:14:12 +0500

    hw5 add AbstractStorage

---
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -0,0 +1,61 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.exception.ExistStorageException;
+import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.Resume;
+
+
+public abstract class AbstractStorage implements Storage {
+
+    protected abstract void doSave(Resume resume, Object searchKey);
+
+    protected abstract void doUpdate(Resume resume, Object searchKey);
+
+    protected abstract void doDelete(Object searchKey);
+
+    protected abstract Object getSearchKey(String uuid);
+
+    protected abstract boolean isExist(Object searchKey);
+
+    protected abstract Resume doGet(Object searchKey);
+
+    public void save(Resume resume) {
+        Object searchKey = getSearchKey(resume.getUuid());
+        if (isExist(searchKey)) {
+            throw new ExistStorageException(resume.getUuid());
+        } else {
+            doSave(resume, searchKey);
+        }
+    }
+
+
+    public void delete(String uuid) {
+        Object searchKey = getSearchKey(uuid);
+        if (!isExist(searchKey)) {
+            throw new NotExistStorageException(uuid);
+        } else {
+            doDelete(searchKey);
+        }
+    }
+
+
+    public void update(Resume resume) {
+        Object searchKey = getSearchKey(resume.getUuid());
+        if (!isExist(searchKey)) {
+            throw new NotExistStorageException(resume.getUuid());
+        } else {
+            doUpdate(resume, searchKey);
+        }
+    }
+
+
+    public Resume get(String uuid) {
+        Object searchKey = getSearchKey(uuid);
+        if (!isExist(searchKey)) {
+            throw new NotExistStorageException(uuid);
+        }
+        return doGet(searchKey);
+    }
+}
+
--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -13,6 +13,7 @@ import static org.junit.Assert.*;
 public abstract class AbstractArrayStorageTest {
     private final Storage storage;
 
+
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
