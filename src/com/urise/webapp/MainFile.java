c152bf04b99d90eb5a46fac4f1223816cd72d899
Alexander Savelev <alex.savelev21@yandex.ru>
2023-07-09 17:29:48 +0500

    hw9 update MainFile

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -7,29 +7,31 @@ import java.io.IOException;
 public class MainFile {
     public static void main(String[] args) throws IOException {
 
-        File root = new File("D:\\JAVA\\BaseJava\\base_java\\src\\com\\urise\\webapp");
+        File dir = new File("D:\\JAVA\\BaseJava\\base_java\\src\\com\\urise\\webapp");
 
-        System.out.println(root.isDirectory());
-        String[] list = root.list();
+        System.out.println(dir.isDirectory());
+        String[] list = dir.list();
         if (list != null) {
             for (String name : list) {
                 System.out.println(name);
             }
         }
-        showDirectory(root);
+        showDirectory(dir,"");
     }
 
-        public static void showDirectory(File root){
-            File[] listFiles = root.listFiles();
-            if(listFiles != null){
+    public static void showDirectory(File dir, String shift) {
+        File[] listFiles = dir.listFiles();
+        if (listFiles != null) {
             for (File file : listFiles) {
                 if (file.isFile()) {
-                    System.out.println("File: " + file.getName());
+                    System.out.println(shift + "File: " + file.getName());
                 } else if (file.isDirectory()) {
-                    System.out.println("Directory: " + file.getName());
-                    showDirectory(file);
+                    System.out.println(shift + "Dir: " + file.getName());
+                    showDirectory(file, " ");
                 }
             }
+        } else {
+            System.out.println(dir.getName() + " is not a folder");
         }
     }
 }
