14ebb4dd454496672e62ecfc0531a0223ea06a08
Alexander Savelev <alex.savelev21@yandex.ru>
2023-07-22 18:36:07 +0500

    hw10 DataStreamSerializer with functional interfaces updated

--- src/com/urise/webapp/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/serializer/DataStreamSerializer.java
@@ -32,15 +32,14 @@ public class DataStreamSerializer implements Serializer {
     }
 
 
-
     @Override
     public Resume doRead(InputStream is) throws IOException {
         try (DataInputStream dis = new DataInputStream(is)) {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
-            readWithException(dis, ()-> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
-            readWithException(dis, ()-> readSection(resume, dis));
+            readWithException(dis, () -> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
+            readWithException(dis, () -> readSection(resume, dis));
 
             return resume;
         }
@@ -62,33 +61,32 @@ public class DataStreamSerializer implements Serializer {
 
     private void writeContentSection(DataOutputStream dos, ContentSection contentSection) throws IOException {
         List<String> elements = contentSection.getElements();
-        dos.writeInt(elements.size());
-        for (String s : elements) {
-            dos.writeUTF(s);
-        }
+        writeWithException(dos, elements, str -> dos.writeUTF(str));
     }
 
     private void writeCompanySection(DataOutputStream dos, CompanySection companySection) throws IOException {
         List<Company> companies = companySection.getCompanies();
-        dos.writeInt(companies.size());
-        for (Company company : companies) {
+        writeWithException(dos, companies, company -> {
             dos.writeUTF(company.getName());
             dos.writeUTF(company.getWebSite());
+
             List<Position> positions = company.getPositions();
-            dos.writeInt(positions.size());
-            for (Position position : positions) {
+            writeWithException(dos, positions, position -> {
                 writeLocalDate(dos, position.getStartDate());
                 writeLocalDate(dos, position.getEndDate());
                 dos.writeUTF(position.getTitle());
                 dos.writeUTF(position.getDescription());
-            }
-        }
+
+            });
+        });
     }
+
 
     private void writeLocalDate(DataOutputStream dos, LocalDate localDate) throws IOException {
         dos.writeInt(localDate.getYear());
         dos.writeInt(localDate.getMonth().getValue());
     }
+
     private LocalDate readLocalDate(DataInputStream dis) throws IOException {
         return LocalDate.of(dis.readInt(), dis.readInt(), 1);
     }
@@ -110,32 +108,30 @@ public class DataStreamSerializer implements Serializer {
     }
 
     private void addContentSection(Resume resume, DataInputStream dis, SectionType sectionType) throws IOException {
-        int size = dis.readInt();
         List<String> items = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            items.add(dis.readUTF());
-        }
+        readWithException(dis, ()-> items.add(dis.readUTF()));
         resume.addSection(sectionType, new ContentSection(items));
     }
 
     private void addCompanySection(Resume resume, DataInputStream dis, SectionType sectionType) throws IOException {
         List<Company> companies = new ArrayList<>();
-        int size = dis.readInt();
-        for (int i = 0; i < size; i++) {
+        readWithException(dis, () -> {
             String name = dis.readUTF();
             String webSite = dis.readUTF();
+
             List<Position> positions = new ArrayList<>();
-            int posSize = dis.readInt();
-            for (int j = 0; j < posSize; j++) {
+            readWithException(dis, () -> {
                 LocalDate startDate = readLocalDate(dis);
                 LocalDate endDate = readLocalDate(dis);
                 String title = dis.readUTF();
                 String description = dis.readUTF();
+
                 positions.add(new Position(startDate, endDate, title, description));
-            }
+            });
             companies.add(new Company(name, webSite, positions));
-        }
+        });
         resume.addSection(sectionType, new CompanySection(companies));
+
     }
 
     private interface collectionWriter<T> {
